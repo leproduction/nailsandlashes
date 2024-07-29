b448c6f44ff589837610631de56d957d7d1b8ff6
leproduction <108595211+leproduction@users.noreply.github.com>
2024-07-29 15:48:18 -0400

    Update Footer.js

--- Client/src/Component/Footer.js
+++ Client/src/Component/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function FooterPage() {
   return (
-    <footer style={{ background: "rgba(189,195,199, 0.3)" }} className="shadow footer-copyright rounded text-center py-3" target="blank">© 2024 GoiGiup Copyright, All rights reserved.
+    <footer style={{ background: "rgba(189,195,199, 0.3)" }} className="shadow footer-copyright rounded text-center py-3" target="blank">© 2024 Hau Le Copyright, All rights reserved.
 
 </footer>
   )
